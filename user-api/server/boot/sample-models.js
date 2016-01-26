be50cffa12a9648d101014a916b502f36eff2560
Humberto Streb <hstreb@gmail.com>
2016-01-26 14:30:26 -0200

    Added authentication

---
+++ user-api/server/boot/sample-models.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+/*
+  var User = app.models.User;
+  User.create({username: 'username', email: 'username@email.com',
+      password: 'supersecret'}, function(err, user) {
+    console.log(user);
+  });
+*/
+  // login
+  // {
+  //   "username": "username",
+  //   "password": "supersecret",
+  //   "ttl": 31556926
+  // }
+};
