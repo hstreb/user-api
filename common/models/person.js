ec230b30aa87c36ae72640b379206db3b6cd6070
Humberto Streb <hstreb@gmail.com>
2016-01-27 15:37:37 -0200

    Added birthdays method

--- common/models/person.js
+++ common/models/person.js
@@ -1,3 +1,81 @@
 module.exports = function(Person) {
+  Person.anniversary = function(cb) {
+    Person.getDataSource().connector.connect(function(err, db) {
+      var collection = db.collection('person');
+      var now = new Date();
+      var day = now.getDate();
+      var month = now.getMonth() + 1;
+      var year = now.getYear();
+      collection.aggregate([
+      {
+        $project: {
+          "name": 1,
+          "userLdap": 1,
+          "userSlack": 1,
+          "month": {
+            "$month": "$birthday"
+          },
+          "day": {
+            "$dayOfMonth": "$birthday"
+          },
+          "year": {
+            "$year": "$birthday"
+          },
+          "active": 1
+        }
+      },
+      {
+        $match:{
+          "month":month,
+          "day":day,
+          "active":true
+        }
+      }], cb);
+    });
+  };
 
+  Person.remoteMethod('birthdays', {
+    http: { path: '/birthdays', verb: 'get' },
+    returns: { arg: 'birthdays', type: 'array' }
+  });
+
+  Person.admissionAnniversary = function(cb) {
+    Person.getDataSource().connector.connect(function(err, db) {
+      var collection = db.collection('person');
+      var now = new Date();
+      var day = now.getDate();
+      var month = now.getMonth() + 1;
+      var year = now.getYear();
+      collection.aggregate([
+      {
+        $project: {
+          "name": 1,
+          "userLdap": 1,
+          "userSlack": 1,
+          "month": {
+            "$month": "$admission"
+          },
+          "day": {
+            "$dayOfMonth": "$admission"
+          },
+          "year": {
+            "$year": "$admission"
+          },
+          "active": 1
+        }
+      },
+      {
+        $match:{
+          "month":month,
+          "day":day,
+          "active":true
+        }
+      }], cb);
+    });
+  };
+
+  Person.remoteMethod('admissionAnniversary', {
+    http: { path: '/admission-anniversary', verb: 'get' },
+    returns: { arg: 'admission-anniversary', type: 'array' }
+  });
 };
